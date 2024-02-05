85f7f936dbf7c6dd64ba59daba6d8b0933a25683
JonasNJensen <102585691+JonasNJensen@users.noreply.github.com>
2024-02-05 15:17:43 +0100

    game manager save

--- Idle Runeterra/Assets/GameManager.cs
+++ Idle Runeterra/Assets/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public TeamManager allyTeamManager;
+    public TeamManager enemyTeamManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,16 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    void startGame(List<champion> allyChampions, List<champion> enemyChampions)
+    {
+        allyTeamManager = new TeamManager();
+        enemyTeamManager = new TeamManager();
+        allyTeamManager.champions = allyChampions;
+        enemyTeamManager.champions = enemyChampions;
+    }
+    void endGame()
+    {
+
     }
 }
