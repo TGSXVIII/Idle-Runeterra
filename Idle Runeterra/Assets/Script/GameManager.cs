749a228bcaa7ea30c0c7cf31d88cc0da7adb6771
JonasNJensen <102585691+JonasNJensen@users.noreply.github.com>
2024-02-06 09:00:03 +0100

    removed test state

--- Idle Runeterra/Assets/Script/GameManager.cs
+++ Idle Runeterra/Assets/Script/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startGame(new List<Champion>(), new List<Champion>());
+        
     }
 
     // Update is called once per frame
