74803fc6b9897fa72840f770559742211b4c8e5f
JonasNJensen <102585691+JonasNJensen@users.noreply.github.com>
2024-02-06 10:32:20 +0100

    made functions public

--- Idle Runeterra/Assets/Script/GameManager.cs
+++ Idle Runeterra/Assets/Script/GameManager.cs
@@ -9,24 +9,14 @@ public class GameManager : MonoBehaviour
     public TeamManager allyTeamManager;
     public TeamManager enemyTeamManager;
     public SceneAsset battleScene;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void startGame(List<Champion> allyChampions, List<Champion> enemyChampions)
+  
+    public void startGame(List<Champion> allyChampions, List<Champion> enemyChampions)
     {
         allyTeamManager = new TeamManager(allyChampions);
         enemyTeamManager = new TeamManager(enemyChampions);
         SceneManager.LoadSceneAsync(battleScene.name, LoadSceneMode.Additive);
     }
-    void endGame()
+    public void endGame()
     {
         SceneManager.UnloadSceneAsync(battleScene.name);
     }
